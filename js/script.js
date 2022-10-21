72b73289a92ea5ec170837db49ea16cad957abc7
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-21 14:06:25 -0400

    everything

--- js/script.js
+++ js/script.js
@@ -1,54 +1,23 @@
-var GHPATH = "/github-page-pwa"
-var APP_PREFIX = "gppwa_"
-var VERSION = "version_001"
-var URLS = [
-  `${GHPATH}/`,
-  `${GHPATH}/index.html`,
-  `${GHPATH}/css/styles.css`,
-  `${GHPATH}/android-chrome-512x512.png`,
-  `${GHPATH}/js/script.js`,
-]
+// Copyright (c) 2022 Timothy Manwell All rights reserved
+//
+// Created by: Timothy Manwell
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
 
-var CACHE_NAME = APP_PREFIX + VERSION
-self.addEventListener("fetch", function (e) {
-  console.log("Fetch request : " + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        console.log("Responding with cache : " + e.request.url)
-        return request
-      } else {
-        console.log("File is not cached, fetching : " + e.request.url)
-        return fetch(e.request)
-      }
-    })
-  )
-})
+"use strict"
 
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Installing cache : " + CACHE_NAME)
-      return cache.addAll(URLS)
-    })
-  )
-})
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
 
-self.addEventListener("activate", function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      cacheWhitelist.push(CACHE_NAME)
-      return Promise.all(
-        keyList.map(function (key, i) {
-          if (cacheWhitelist.indexOf(key) === -1) {
-            console.log("Deleting cache : " + keyList[i])
-            return caches.delete(keyList[i])
-          }
-        })
-      )
-    })
-  )
-})
+/**
+ * This function displays an alert.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
